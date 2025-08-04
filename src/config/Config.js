5abcffd5847eb29f2f1b1c4b0a981cf951e90e3f
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2025-08-04 09:54:31 -0500

    Updates config for 2025-26
    Migrates spreadsheet IDs to new 2025-26 version and updates corresponding URLs and references throughout the configuration.
    
    Adds TODO comment for contact info range update and includes documentation comments for range constants to clarify their spreadsheet targets.

--- src/config/Config.js
+++ src/config/Config.js
@@ -16,8 +16,8 @@ const CONFIG = {
   SPREADSHEETS: {
     MAIN: "1uCQ_Z4QLbHq89tutZ4Wen0TREwS8qEx2j7MmzUgXOaY",
     NAHS_CRITERIA: "1gaGyH312ad85wpyfH6dGbyNiS4NddqH6NvzTG6RPGPA",
-    NAMS_CRITERIA: "1O3DSgTbhphNVDXLmlGkEiyVejsL_l4fPsf2cJJpQpTo",
-    NAHS_TRANSITION_NOTES: "14-nvlNOLWebnJJOQNZPnglWx0OuE5U-_xEbXGodND6E"
+    NAMS_CRITERIA: "1q61g_br0jmqtAvyQhNYto1jezfzjttmsKAAG-pznXak",
+    NAHS_TRANSITION_NOTES: "14-nvlNOLWebnJJOQNZPnglWx0OuE5U-_xEbXGodND6E",
   },
 
   /**
@@ -25,20 +25,23 @@ const CONFIG = {
    */
   EMAIL_CONFIGS: [
     {
-      label: "Campuses/NAHS/Transition Information Workflow Project/Transition Information Workflow Project Schedules",
+      label:
+        "Campuses/NAHS/Transition Information Workflow Project/Transition Information Workflow Project Schedules",
       sheetName: "Schedules",
-      rangeToClear: "A2:O"
+      rangeToClear: "A2:O",
     },
     {
-      label: "Campuses/NAHS/Transition Information Workflow Project/Transition Information Workflow Project Contact Information",
+      label:
+        "Campuses/NAHS/Transition Information Workflow Project/Transition Information Workflow Project Contact Information",
       sheetName: "ContactInfo",
-      rangeToClear: "A2:J"
+      rangeToClear: "A2:J", // @todo Update to use new column organization from the report. Go to the email and check the new column orders.
     },
     {
-      label: "Campuses/NAHS/Transition Information Workflow Project/Transition Informatin Workflow Entry_Withdrawal",
+      label:
+        "Campuses/NAHS/Transition Information Workflow Project/Transition Informatin Workflow Entry_Withdrawal",
       sheetName: "Entry_Withdrawal2",
-      rangeToClear: "A2:O"
-    }
+      rangeToClear: "A2:O",
+    },
   ],
 
   /**
@@ -46,34 +49,34 @@ const CONFIG = {
    */
   PUSH_DATA_CONFIGS: {
     sourceSheets: {
-      "Alt_HS_Attendance_Enrollment_Count": {
+      Alt_HS_Attendance_Enrollment_Count: {
         range: "A2:H",
         targets: [
           {
             spreadsheetId: "1gaGyH312ad85wpyfH6dGbyNiS4NddqH6NvzTG6RPGPA",
-            sheetName: "Alt_HS_Attendance_Enrollment_Count"
-          }
-        ]
+            sheetName: "Alt_HS_Attendance_Enrollment_Count",
+          },
+        ],
       },
-      "Entry_Withdrawal": {
+      Entry_Withdrawal: {
         range: "A2:I",
         targets: [
           {
             spreadsheetId: "1gaGyH312ad85wpyfH6dGbyNiS4NddqH6NvzTG6RPGPA",
-            sheetName: "Entry_Withdrawal"
-          }
-        ]
+            sheetName: "Entry_Withdrawal",
+          },
+        ],
       },
-      "Allergies": {
+      Allergies: {
         range: "A2:E",
         targets: [
           {
-            spreadsheetId: "1O3DSgTbhphNVDXLmlGkEiyVejsL_l4fPsf2cJJpQpTo",
-            sheetName: "Allergies"
-          }
-        ]
-      }
-    }
+            spreadsheetId: "1q61g_br0jmqtAvyQhNYto1jezfzjttmsKAAG-pznXak",
+            sheetName: "Allergies",
+          },
+        ],
+      },
+    },
   },
 
   /**
@@ -82,7 +85,7 @@ const CONFIG = {
   RETRY_CONFIG: {
     maxRetries: 5,
     retryDelay: 1000, // milliseconds
-    fileCleanupDelay: 2000 // milliseconds
+    fileCleanupDelay: 2000, // milliseconds
   },
 
   /**
@@ -91,7 +94,7 @@ const CONFIG = {
   SETTINGS: {
     timezone: "America/Chicago",
     dateFormat: "MM/dd/yyyy",
-    logLevel: "INFO"
+    logLevel: "INFO",
   },
 
   /**
@@ -100,17 +103,17 @@ const CONFIG = {
   SUCCESS_LINKS: [
     {
       name: "NAHS Criteria Sheet",
-      url: "https://docs.google.com/spreadsheets/d/1gaGyH312ad85wpyfH6dGbyNiS4NddqH6NvzTG6RPGPA/edit?gid=524766497#gid=524766497"
+      url: "https://docs.google.com/spreadsheets/d/1gaGyH312ad85wpyfH6dGbyNiS4NddqH6NvzTG6RPGPA/edit?gid=644557240#gid=644557240",
     },
     {
-      name: "NAMS 2024-25 Criteria Sheet",
-      url: "https://docs.google.com/spreadsheets/d/1O3DSgTbhphNVDXLmlGkEiyVejsL_l4fPsf2cJJpQpTo/edit?gid=93840204#gid=93840204"
+      name: "NAMS 2025-26 Criteria Sheet",
+      url: "https://docs.google.com/spreadsheets/d/1q61g_br0jmqtAvyQhNYto1jezfzjttmsKAAG-pznXak/edit?gid=1453553637#gid=1453553637",
     },
     {
       name: "NAHS 24-25 Student Transition Notes",
-      url: "https://docs.google.com/spreadsheets/d/14-nvlNOLWebnJJOQNZPnglWx0OuE5U-_xEbXGodND6E/edit?gid=1422083122#gid=1422083122"
-    }
-  ]
+      url: "https://docs.google.com/spreadsheets/d/14-nvlNOLWebnJJOQNZPnglWx0OuE5U-_xEbXGodND6E/edit?gid=1422083122#gid=1422083122",
+    },
+  ],
 };
 
 /**
@@ -125,12 +128,12 @@ const MIME_TYPES = {
  * Common range patterns
  */
 const RANGES = {
-  SCHEDULES_CLEAR: "A2:O",
-  CONTACT_INFO_CLEAR: "A2:J",
-  ENTRY_WITHDRAWAL_CLEAR: "A2:O",
-  ALT_HS_ATTENDANCE: "A2:H",
-  ENTRY_WITHDRAWAL_MANUAL: "A2:I",
-  ALLERGIES: "A2:E"
+  SCHEDULES_CLEAR: "A2:O", // Range refers to the Schedules sheet in the _DataLake spreadsheet
+  CONTACT_INFO_CLEAR: "A2:N", // Range refers to the ContactInfo sheet in the _DataLake spreadsheet
+  ENTRY_WITHDRAWAL_CLEAR: "A2:O", // Range refers to the Entry_Withdrawal2 sheet in the _DataLake spreadsheet
+  ALT_HS_ATTENDANCE: "A2:H", // Range refers to the Alt_HS_Attendance_Enrollment_Count sheet in the NAHS Criteria spreadsheet
+  ENTRY_WITHDRAWAL_MANUAL: "A2:I", // Range refers to the Entry_Withdrawal sheet in the NAHS Criteria spreadsheet
+  ALLERGIES: "A2:E" // Range refers to the Allergies sheet in the NAMS 2025-26 Criteria spreadsheet
 };
 
 /**
